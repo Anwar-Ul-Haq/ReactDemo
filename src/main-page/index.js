6d38f73ac30d76ec8a5061bca3c6cba50b562aac
Anwar Ul Haq <aulhaq6@gmail.com>
2021-10-03 22:17:18 +0100

    First component for the application

---
+++ src/main-page/index.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
