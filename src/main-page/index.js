4c27ee37acead78d74c9889cc0736f177c09a809
Anwar Ul Haq <aulhaq6@gmail.com>
2021-10-05 23:38:06 +0100

    Added ReactHooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
+
 import './main-page.css';
 import Header from './header';
+import { useEffect ,useState , useMemo} from 'react';
+
 
 
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+  
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+
+  }, []);
+
+ const featureHouse = useMemo(() => {
+
+    if (allHouses.length) {
+    
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return  allHouses[randomIndex];
+    }
+  
+  
+} , [allHouses])
+
+  
+
   return (
     <div className="container">
       <Header subtitle="Providing houses all over the world"
